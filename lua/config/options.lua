fb88010820087102034d6504298a749c4b83f7d0
Jonatan Westling <jonatanwestling4@gmail.com>
2026-02-12 22:42:41 +0100

    feat: global indent/spaces settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,14 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker = "telescope"
+
+-- Tab / indentation defaults
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+
+-- Helpers
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
